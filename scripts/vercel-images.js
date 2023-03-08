68d4d198c36292869703a23ce1599393584250ae
aakash_goplani_nagarro <aakash.goplani@nagarro.com>
2023-03-09 00:45:20 +0530

    .

--- scripts/vercel-images.js
+++ scripts/vercel-images.js
@@ -1,15 +1,11 @@
 import fs from 'node:fs';
-// import dotenv from 'dotenv';
-// dotenv.config();
-// sizes should be shared variable with Image component
-// import { sizes } from './theme.cjs';
 
 const file = '.vercel/output/config.json';
 
 const config = {
 	...JSON.parse(fs.readFileSync(file, 'utf-8')),
 	images: {
-		sizes: [640, 768, 1024, 1280, 1536], // sizes
+		sizes: [640, 768, 1024, 1280, 1536],
 		domains: [],
 		minimumCacheTTL: 60,
 		formats: ['image/avif', 'image/webp', 'image/jpeg', 'image/jpg', 'image/png']
